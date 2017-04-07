b70ee5f3031f97cd1058ffd52b30a6d1c31e1504
Dean Smith <Dean Smith>
2017-04-07 00:44:34 -0500

    Commenting

--- dictionaries/stocks.py
+++ dictionaries/stocks.py
@@ -1,14 +1,17 @@
+#This is a dictionary. It contains "key value pairs". I am setting
+#value to each ticker
 stock = {"rem": "Remington", "win": "Winchester", "be": "Bear",
 	"pse": "PSE"}
-
+#This is a list of tuples inside it. Tuples are a form of list,
+#they just connot be changed.
 purchases = [("rem", 200, "1/14/2010", 48),
 	("win", 170, "7/11/1999", 30),
 	("be", 150, "3/9/2017", 1),
 	("win", 145, "3/7/2000", 7),
 	("pse", 200, "8/9/2000", 90)]
-
+#looping through each purchase.
 for purchase in purchases:
-	ticker = purchase[0]
+	ticker = purchase[0]#making ticker be the value of the first argument.
 
 	number_of_shares = purchase[1]
 	purchase_price = purchase[3]
